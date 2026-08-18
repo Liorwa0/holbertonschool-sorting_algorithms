dbd29ffca02f7caf6f65dc98f05cec75290b3d02
zaslse <zaslse@hotmail.com>
2026-08-18 00:33:39 +0000

    Task 1: Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - Swaps two nodes in a doubly linked list
+ * @list: Double pointer to the head of the list
+ * @node1: First node to swap
+ * @node2: Second node to swap
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1->prev)
+		node1->prev->next = node2;
+	else
+		*list = node2;
+
+	if (node2->next)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort algorithm
+ * @list: Double pointer to the head of the doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *insertion_ptr, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current->next;
+		insertion_ptr = current->prev;
+
+		while (insertion_ptr != NULL && insertion_ptr->n > current->n)
+		{
+			swap_nodes(list, insertion_ptr, current);
+			print_list(*list);
+			insertion_ptr = current->prev;
+		}
+		current = temp;
+	}
+}
