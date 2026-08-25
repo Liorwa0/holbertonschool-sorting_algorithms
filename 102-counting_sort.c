d8a922d9b98fa62255d21fe6a367cdd7bb39491b
Arwa alhomrani <lla644183@gmail.com>
2026-08-26 01:29:12 +0300

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,46 +1,73 @@
+#include <stdlib.h>
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array using Counting sort algorithm
- * @array: The array to sort
- * @size: Size of the array
+ * get_max - Returns the maximum integer in an array
+ * @array: An array of integers
+ * @size: The size of the array
+ *
+ * Return: The maximum integer in the array
  */
-void counting_sort(int *array, size_t size)
+int get_max(int *array, size_t size)
 {
-	int *count, *output, max, i;
+	int max;
+	size_t i;
 
-	if (array == NULL || size < 2)
-		return;
-	output = malloc(sizeof(int) * size);
-	if (output == NULL)
-		return;
 	max = array[0];
-	for (i = 1; i < (int)size; i++)
+	for (i = 1; i < size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
 	}
+	return (max);
+}
+
+/**
+ * counting_sort - Sorts an array of integers in ascending order
+ *                 using the Counting sort algorithm
+ * @array: An array of integers
+ * @size: The size of the array
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *count, *output, max;
+	size_t i;
+
+	if (!array || size < 2)
+		return;
+
+	max = get_max(array, size);
 	count = malloc(sizeof(int) * (max + 1));
-	if (count == NULL)
+	if (!count)
+		return;
+
+	for (i = 0; i <= (size_t)max; i++)
+		count[i] = 0;
+
+	for (i = 0; i < size; i++)
+		count[array[i]] += 1;
+
+	for (i = 1; i <= (size_t)max; i++)
+		count[i] += count[i - 1];
+
+	print_array(count, max + 1);
+
+	output = malloc(sizeof(int) * size);
+	if (!output)
 	{
-		free(output);
+		free(count);
 		return;
 	}
-	for (i = 0; i <= max; i++)
-		count[i] = 0;
-	for (i = 0; i < (int)size; i++)
-		count[array[i]] += 1;
-	for (i = 1; i <= max; i++)
-		count[i] += count[i - 1];
-	print_array(count, max + 1);
-	for (i = (int)size - 1; i >= 0; i--)
+
+	for (i = 0; i < size; i++)
 	{
 		output[count[array[i]] - 1] = array[i];
 		count[array[i]] -= 1;
 	}
-	for (i = 0; i < (int)size; i++)
+
+	for (i = 0; i < size; i++)
 		array[i] = output[i];
-	free(output);
+
 	free(count);
+	free(output);
 }
-/* Counting sort review */
